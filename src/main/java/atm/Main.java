634ac4ecc23f14b7ea7a82d30520f9855b6618d7
SukritaEarn <sukrita.ki@ku.th>
2020-09-23 17:49:14 +0700

    add spring and config

--- src/main/java/atm/Main.java
+++ src/main/java/atm/Main.java
@@ -1,12 +1,14 @@
 package atm;
 
+import atm.config.ATMConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.ApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        DataSourceDB dataSourceDB = new DataSourceDB();
-        DataSourceFile dataSourceFile = new DataSourceFile("customers.txt");
-        Bank bank = new Bank("My Bank", dataSourceFile);
-        ATM atm = new ATM(bank);
-        AtmUI atmUI = new AtmUI(atm);
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ATMConfig.class);
+        AtmUI atmUI = context.getBean(AtmUI.class);
         atmUI.run();
     }
 }
